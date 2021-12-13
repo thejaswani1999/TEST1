2fb41eee51004cc402e121b8e3092bf9f6ce6538
Thejaswani586 <sampathreddy.sampredd@gmail.com>
2021-12-13 10:49:19 +0530

    first commit

---
+++ ts1.js
@@ -0,0 +1,23 @@
+function myFunction()
+{
+   // var text1=document.getElementById("button").innerHTML;
+    var x=document.createElement("li");
+    var y=document.createTextNode(document.getElementById("fname").value);
+    x.appendChild(y);
+    document.getElementById("ol-tag").appendChild(x);
+
+}
+function dearFunction()
+{
+
+}
+
+function emptyList()
+{
+    
+
+}
+function saveList()
+{
+
+}
